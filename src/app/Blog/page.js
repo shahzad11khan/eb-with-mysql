6dcc77335ec29cbc54ff98b815866098ef3c8bfe
shahzad11khan <zarakali401@gmail.com>
2025-08-15 10:48:42 +0500

    update the code

--- src/app/Blog/page.js
+++ src/app/Blog/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useCallback  } from "react";
 import Top from "../Utils/Top";
 import Image from "next/image";
 import { BlogsCount } from "../AdminDashboard/components/ShowApidatas/ShowUserAPiDatas";
